550cfa490f87b2f36e33daed0a0c981d34f72fb6
Surabhi Mahale <surabhimahale12@gmail.com>
2024-02-08 00:29:11 +0530

    Update Header component and index.css

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { AppBar } from '@mui/material';
+import { Toolbar } from '@mui/material';
+import Logo from './shared/logo';
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+     <AppBar sx ={{bgcolor: 'transparent', position: 'static', boxShadow: 'none'}}>
+        <Toolbar sx={{ display: "flex" }}>
+        <Logo/>
+        </Toolbar>
+    </AppBar>
+  );
 }
 
 export default Header

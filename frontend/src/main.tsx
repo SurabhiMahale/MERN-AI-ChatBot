35979f5e0599619c557f5b0505e3251e13c31052
Surabhi Mahale <surabhimahale12@gmail.com>
2024-02-08 01:08:25 +0530

    Add AuthProvider to main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { AuthProvider } from './context/AuthContext.tsx'
 import { BrowserRouter } from "react-router-dom";
 import { createTheme, ThemeProvider } from '@mui/material/styles'
 const theme = createTheme({
@@ -13,11 +14,12 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <AuthProvider>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
     </BrowserRouter>
-
+    </AuthProvider>
   </React.StrictMode>,
 )
